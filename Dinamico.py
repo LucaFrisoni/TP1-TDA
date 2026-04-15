edfe5422369f1533e9d0c11b6c7dda4387c40d77
Valentina Díaz Racioppi <167375320+Valudiaz78@users.noreply.github.com>
2026-04-14 21:13:36 -0300

    Update Dinamico.py

--- Dinamico.py
+++ Dinamico.py
@@ -10,23 +10,45 @@ etc.
 Desarrollar un algoritmo de programación dinámica que encuentre el menor número de
 palíndromos que forman una cadena dada. Por ejemplo, para ARACALACANA debería
 devolver 3.
+
+
 """
 
-def palindromos_dinamico(palabra):
-    if len(palabra) == 0:
-        return 0
-    if len(palabra) == 1:
-        return 1
-    letras_actuales = []
-    palindromos = 0
-    for letra in palabra:
-        if len(letras_actuales) == 0:
-            letras_actuales.append(letra)
-        elif letra == letras_actuales[1:]:
-            letras_actuales.append(letra)
-        else:
-            letras_actuales = []
-            palindromos+=1
-    return palindromos
 
-print(palindromos_dinamico(ARACALACANA))
+def tabla_palindromos(palabra):
+    n = len(palabra)
+    pal = [[False] * n for _ in range(n)]
+
+    for i in range(n):
+        pal[i][i] = True
+
+    for i in range(n - 1):
+        pal[i][i + 1] = palabra[i] == palabra[i + 1]
+
+    for largo in range(3, n + 1):
+        for i in range(0, n - largo + 1):
+            j = i + largo - 1
+            pal[i][j] = palabra[i] == palabra[j] and pal[i + 1][j - 1]
+
+    return pal
+
+
+
+def palindromos_dinamico(palabra):
+    n = len(palabra)
+    pal = tabla_palindromos(palabra)
+    if n == 0:
+        return 0
+
+    M = [float("inf")] * (n + 1)
+    M[0] = 0
+
+    for i in range(1, n + 1):
+        for j in range(0, i):
+            if pal[j][i - 1]:
+                M[i] = min(M[i], M[j] + 1)
+
+    return M[n]
+
+print(palindromos_dinamico("ARACALACANA"))
+
